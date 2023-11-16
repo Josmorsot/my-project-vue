38b9e3cb359c36d4f857ac60274b82a6bae5c212
Ivan <imoreno@carto.com>
2023-11-16 09:33:57 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+function execDataObservatoryCatalog (el) {
+  if (document.querySelector(el)) {
+    const app = new Vue({
+      el,
+      components: { App },
+      template: '<App />'
+    });
+
+    return { app };
+  }
+}
+
+const el = process.env.NODE_ENV === 'development' ? '#app' : '#do-catalog';
+execDataObservatoryCatalog(el);
+
+window.createDataObservatoryCatalog = execDataObservatoryCatalog;
+
